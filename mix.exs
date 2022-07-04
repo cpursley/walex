4d0ff8deb1cd3c6ee249ba0a8f88081cf2e3d99f
Chase Pursley <chasepursley@gmail.com>
2022-07-04 11:10:47 -0400

    epgsql_server improvements & troubleshooting - take II.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WalEx.MixProject do
   def project do
     [
       app: :walex,
-      version: "0.7.1",
+      version: "0.7.2",
       elixir: "~> 1.13",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
