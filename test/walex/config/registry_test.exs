5fab37285012fe3c4ce8ac31a8b429de41479c34
Chase Pursley <chasepursley@gmail.com>
2024-01-03 17:59:36 -0500

    Tests: Dont test in setups - take 2.

--- test/walex/config/registry_test.exs
+++ test/walex/config/registry_test.exs
@@ -42,14 +42,7 @@ defmodule WalEx.Config.RegistryTest do
     test "should set agent state" do
       name = WalExRegistry.set_name(:set_agent, __MODULE__, :app_name_test)
 
-      configs = [
-        my_atom: :test_config,
-        my_number: 99,
-        my_string: "test config",
-        my_map: %{john: :doe},
-        my_list: [1, 9, 9, 4]
-      ]
-
+      configs = []
       Agent.start_link(fn -> configs end, name: name)
 
       assert configs == WalExRegistry.get_state(:get_agent, __MODULE__, :app_name_test)
