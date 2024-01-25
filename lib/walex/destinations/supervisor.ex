4b59ecdcd50dfc7fb402b3754ab28d5581e98461
Dohan Kim <hodoli2776@paran.com>
2024-01-26 03:06:18 +0900

    use start_link/3 in other supervisors

--- lib/walex/destinations/supervisor.ex
+++ lib/walex/destinations/supervisor.ex
@@ -11,14 +11,13 @@ defmodule WalEx.Destinations.Supervisor do
     app_name = Keyword.get(opts, :app_name)
     name = Config.Registry.set_name(:set_supervisor, __MODULE__, app_name)
 
-    Supervisor.start_link(__MODULE__, configs: opts, name: name)
+    Supervisor.start_link(__MODULE__, opts, name: name)
   end
 
   @impl true
   def init(opts) do
     app_name =
       opts
-      |> Keyword.get(:configs)
       |> Keyword.get(:name)
 
     children =
