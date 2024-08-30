ac795250c3985df058a7228d4f1db4d0fd3536d9
Chase Pursley <chasepursley@gmail.com>
2024-08-30 12:42:25 -0400

    forgot version bump in mix.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WalEx.MixProject do
   def project do
     [
       app: :walex,
-      version: "4.1.0",
+      version: "4.2.0",
       elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
